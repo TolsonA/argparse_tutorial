2ab5385ecd41e9309c8e7ffbcfd386dd30e2abb6
TolsonA <drewtol@yahoo.com>
2024-05-22 17:56:11 -0400

    made it to args with choices

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # argparse_tutorial
+
+python prog.py
+python prog.py --help
+usage: prog.py [-h]
+
+options:
+  -h, --help  show this help message and exit
+python prog.py --verbose
+usage: prog.py [-h]
+prog.py: error: unrecognized arguments: --verbose
+python prog.py foo
+usage: prog.py [-h]
+prog.py: error: unrecognized arguments: foo
+
---
+++ prog.py
@@ -0,0 +1,58 @@
+import argparse
+"""
+argparse square
+parser = argparse.ArgumentParser()
+parser.add_argument("square", help="display a square of a given number",
+                    type=int)
+args = parser.parse_args()
+print(args.square**2)
+
+shortened optional arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+args = parser.parse_args()
+if args.verbose:
+    print("verbosity turned on")
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--verbose", help="increase output verbosity",
+                    action="store_true")  
+args = parser.parse_args()
+if args.verbose:
+    print("verbosity turned on")
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("square", type=int,
+                    help="display a square of a given number")
+parser.add_argument("-v", "--verbosity", type=int,
+                    help="increase output verbosity")
+args = parser.parse_args()
+answer = args.square**2
+if args.verbosity == 2:
+    print(f"the square of {args.square} equals {answer}")
+elif args.verbosity == 1:
+    print(f"{args.square}^2 == {answer}")
+else:
+    print(answer)
+
+    """
+
+parser = argparse.ArgumentParser()
+parser.add_argument("square", type=int,
+                    help="display a square of a given number")
+parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2],
+                    help="increase output verbosity")
+args = parser.parse_args()
+answer = args.square**2
+if args.verbosity == 2:
+    print(f"the square of {args.square} equals {answer}")
+elif args.verbosity == 1:
+    print(f"{args.square}^2 == {answer}")
+else:
+    print(answer)
